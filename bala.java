092856ace3bb3042af1c84d2b4ad5ebd0b9e0e81
balakarthick160 <43032613+balakarthick160@users.noreply.github.com>
2018-09-07 11:20:52 +0530

    Add files via upload

---
+++ bala.java
@@ -0,0 +1,15 @@
+class Main {
+  public static void main(String[] args) {
+    int a,b,c,d,e,f;
+     a=2;
+    b=6;
+    c=a+b;
+    d=a-b;
+    e=a*b;
+    f=a/b;
+    System.out.println("sum="+c);
+    System.out.println("difference="+d);
+    System.out.println("multi="+e);
+    System.out.println("divide="+f);
+  }
+}
