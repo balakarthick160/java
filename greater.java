5c95641dfc063446b8386bd334d2ef1260c436b1
balakarthick160 <43032613+balakarthick160@users.noreply.github.com>
2018-10-01 15:38:42 +0530

    Add files via upload

---
+++ greater.java
@@ -0,0 +1,15 @@
+class Main
+{
+  public static void main(String arys[]){
+    if (6<3);
+    {
+    System.out.println("6 is greater then 3");
+    }
+    if (6>4);
+    {
+      System.out.println("4 is less then 6");
+    }
+  }
+}
+
+
