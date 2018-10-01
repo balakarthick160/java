ffe9d9670ca36d2865620602248e6f15d0ada302
balakarthick160 <43032613+balakarthick160@users.noreply.github.com>
2018-10-01 15:37:28 +0530

    Add files via upload

---
+++ boolean.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+		String name="Sridevi";
+      String city="Chennai";
+      byte age=32;
+      float height=5.3f;
+      boolean b;
+      b=true;
+      char sex='F';
+      double citypopulation=4828853;
+      System.out.println("Name:"+name);
+      System.out.println("City:"+city);
+      System.out.println("Age:"+age);
+      System.out.println("Height:"+height);
+      System.out.println("Married:"+b);
+      System.out.println("Sex:"+sex);
+      System.out.println("City Population:"+citypopulation);
+
+	}
+}              
