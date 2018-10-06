c100e62444835b2e310a2200f228aa1bd2a4812b
balakarthick160 <43032613+balakarthick160@users.noreply.github.com>
2018-10-06 12:31:07 +0530

    Add files via upload

---
+++ hello world.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    int n;
+    Scanner scan=new Scanner(System.in);
+    n=scan.nextInt();
+   for(int i=0; i<n; i++)
+    System.out.println("Hello ");
+  }
+}
